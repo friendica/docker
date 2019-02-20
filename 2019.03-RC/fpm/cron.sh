cc069ccf8d3a8a98f4e2348be7fefcc23dfb2ab4
Philipp Holzer <admin@philipp.info>
2019-02-20 19:06:44 +0100

    add 2019.03-RC directory

---
+++ 2019.03-RC/fpm/cron.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+trap "break;exit" HUP INT TERM
+
+while [ ! -f /var/www/html/config/local.ini.php ] && [ ! -f /var/www/html/config/local.config.php ]; do
+    sleep 1
+done
+
+# TODO let the database and the autoinstall time to complete - not winning a beauty contest
+sleep 15s
+
+php /var/www/html/bin/daemon.php -f start

841a0b438b5d595bc0de1e90420b34e41285049a
Philipp Holzer <admin+github@philipp.info>
2019-07-19 17:37:44 +0200

    Add dynamic config compatibility - Use APCu cache as default caching mechanism - Add explicit redis environment variables for redis lock-support - Add a lot of environment variables, which aren't part of admin-page - Add possibility to disable URL & E-Mail validation (for demo-page) - Dynamically copy /config/ content without overwriting existing one
    Add opcache support for performance reason
    Add multi-core support for php extensions install
    
    Bump redis version to 5.0.1 (last stable)
    
    Exclude /log/ directory during sync (to avoid deleting logs)

--- update.sh
+++ update.sh
@@ -118,15 +118,13 @@ function create_variant() {
 		s/%%REDIS_VERSION%%/'"${pecl_versions[redis]}"'/g;
 	' "$dir/Dockerfile"
 
-	# Copy the .docker-files to the directories (excluding README.md)
-	for name in ".docker-files"/*; do
-		# Don't copy the README of any directory
-		if [[ "$name" == *.sh || "$name" == *.exclude ]]; then
-			file=${name#".docker-files"}
-			mkdir -p $(dirname $dir/$file)
-			cp -r "$name" "$dir/$file"
-		fi
+	for name in entrypoint cron; do
+		cp "docker-$name.sh" "$dir/$name.sh"
 	done
+
+	cp upgrade.exclude "$dir/"
+
+	cp -rT .config "$dir/config"
 
     travisEnvAmd64='\n    - env: VERSION='"$dockerName"' VARIANT='"$variant"' ARCH=amd64'"$travisEnvAmd64"
 	for arch in i386 amd64; do

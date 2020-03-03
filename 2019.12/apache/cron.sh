de7cdd108f4e0ca6807ad7b6bbda6114a6273ded
valvin <git@valvin.fr>
2020-03-03 20:39:48 +0100

    fix(cron.sh - #105): keep /bin/sh as prent process

--- 2019.12/apache/cron.sh
+++ 2019.12/apache/cron.sh
@@ -7,7 +7,7 @@ done
 
 echo "Waiting for MySQL $MYSQL_HOST initialization..."
 if php /var/www/html/bin/wait-for-connection "$MYSQL_HOST" "${MYSQL_PORT:-3306}" 300; then
-  exec php /var/www/html/bin/daemon.php -f start
+  php /var/www/html/bin/daemon.php -f start
 else
   echo "[ERROR] Waited 300 seconds, no response" >&2
 fi

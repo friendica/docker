b8c192e71100ed20d2c49acb6be70ea72570456e
GitHub Workflow <workflow@github.com>
2021-08-29 01:28:42 +0000

    Runs update.sh

---
+++ 2021.09-rc/fpm-alpine/config/00apcu.config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * If nothing else set, use APCu as a caching driver (best performance for local caching)
+ */
+
+return [
+	'system' => [
+		'cache_driver' => 'apcu',
+		'session_handler' => 'cache',
+	],
+];

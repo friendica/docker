ee990e483762774a241673b91bab9860914a7214
Philipp <admin@philipp.info>
2021-09-03 21:13:29 +0200

    Execute cron as www-data

--- 2021.09-dev/fpm-alpine/cron.sh
+++ 2021.09-dev/fpm-alpine/cron.sh
@@ -8,7 +8,7 @@ done
 echo "Waiting for MySQL $MYSQL_HOST initialization..."
 if php /var/www/html/bin/wait-for-connection "$MYSQL_HOST" "${MYSQL_PORT:-3306}" 300; then
   sh /setup_msmtp.sh
-  exec /sbin/tini -- php /var/www/html/bin/daemon.php -f start
+  exec gosu www-data:www-data tini -- php /var/www/html/bin/daemon.php -f start
 else
   echo "[ERROR] Waited 300 seconds, no response" >&2
 fi

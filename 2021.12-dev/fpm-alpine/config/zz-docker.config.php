b3807c23d74aa0c8b026e7986a432c9f019befc5
Philipp <admin@philipp.info>
2021-12-13 20:12:52 +0100

    Cleanup config - Remove deprecated statements - Move session_handler to right place - Add new distributed cache capability

--- 2021.12-dev/fpm-alpine/config/zz-docker.config.php
+++ 2021.12-dev/fpm-alpine/config/zz-docker.config.php
@@ -20,43 +20,9 @@ $config = [
 	],
 ];
 
-if (getenv('FRIENDICA_TZ')) {
-	$config['config']['timezone'] = getenv('FRIENDICA_TZ');
-}
-
-if (getenv('FRIENDICA_LANG')) {
-	$config['config']['language'] = getenv('FRIENDICA_LANG');
-}
-
-if (getenv('FRIENDICA_ADMIN_MAIL')) {
-	$config['config']['admin_email'] = getenv('FRIENDICA_ADMIN_MAIL');
-}
-
-if (getenv('FRIENDICA_SITENAME')) {
-	$config['config']['sitename'] = getenv('FRIENDICA_SITENAME');
-}
-
 if (!empty(getenv('FRIENDICA_NO_VALIDATION'))) {
 	$config['system']['disable_url_validation'] = true;
 	$config['system']['disable_email_validation'] = true;
-}
-
-if (!empty(getenv('FRIENDICA_DATA'))) {
-	$config['storage']['class'] = \Friendica\Model\Storage\Filesystem::class;
-
-	if (!empty(getenv('FRIENDICA_DATA_DIR'))) {
-		$config['storage']['filesystem_path'] = getenv('FRIENDICA_DATA');
-	}
-}
-
-if (!empty(getenv('FRIENDICA_DEBUGGING'))) {
-	$config['system']['debugging'] = true;
-	if (!empty(getenv('FRIENDICA_LOGFILE'))) {
-		$config['system']['logfile'] = getenv('FRIENDICA_LOGFILE');
-	}
-	if (!empty(getenv('FRIENDICA_LOGLEVEL'))) {
-		$config['system']['loglevel'] = getenv('FRIENDICA_LOGLEVEL');
-	}
 }
 
 if (!empty(getenv('SMTP_DOMAIN'))) {
